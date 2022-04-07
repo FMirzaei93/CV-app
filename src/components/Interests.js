6667b6434b3f9c0c5cb477dab9cde5ce1e7eb85c
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-06 17:15:38 -0700

    Update Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,12 @@
 function Interests() {
   return (
-    <div>
-      <p>Interests</p>
+    <div className='sub-container'>
+      <p className='title'>Interests</p>
+      <p className='explanation'>
+        About I am a frontend developer with a particular interest in making
+        things simple and automating daily tasks. I try to keep up with security
+        and best practices.
+      </p>
     </div>
   );
 }
