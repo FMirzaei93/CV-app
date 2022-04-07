625ec109e9a8145cdd7d70be9c8028b60e4e9e4b
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-06 17:15:29 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 function Footer() {
   return (
-    <div>
-      <p>Footer</p>
+    <div className='div-footer'>
+      <i className='fab fa-twitter-square'></i>
+      <i className='fab fa-facebook-square'></i>
+      <i className='fab fa-instagram-square'></i>
+      <i className='fab fa-github-square'></i>
     </div>
   );
 }
