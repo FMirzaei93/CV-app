c6b54166eae2ea88fcf8eb3d7bee5755f033a769
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-06 17:15:32 -0700

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,21 @@
+import my_image from "../images/me.jpg";
 function Info() {
   return (
-    <div>
-      <p>Info</p>
+    <div className='info-container'>
+      <img className='profile-photo' src={my_image} />
+      <p className='name'>Firoozeh Mirzaei</p>
+      <p className='profession'>Web Developer</p>
+      <p className='website'>firoozehmirzaei.website</p>
+
+      <div className='info-buttons'>
+        <button className='btn-email'>
+          <i className='fa fa-envelope'></i> Email
+        </button>
+
+        <button className='btn-linkedIn'>
+          <i className='fab fa-linkedin'></i> linkedIn
+        </button>
+      </div>
     </div>
   );
 }
