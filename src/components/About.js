9328fc73333724033cc28affab7b301186ad284e
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-06 17:15:26 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
 function About() {
   return (
-    <div>
-      <p>About</p>
+    <div className='sub-container'>
+      <p className='title'>About</p>
+      <p className='explanation'>
+        About I am a frontend developer with a particular interest in making
+        things simple and automating daily tasks.
+      </p>
     </div>
   );
 }
